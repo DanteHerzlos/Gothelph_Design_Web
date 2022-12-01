dd56ef8e69550e3a41837fe6b9a9cdc48404f04f
DanteHerzlos <streat13@gmail.com>
2022-12-02 02:18:58 +0600

    add form and api for uploading category

--- components/AddCategoryForm.tsx
+++ components/AddCategoryForm.tsx
@@ -1,24 +1,48 @@
-import React from 'react'
-import Button from './UI/Button';
-import FileInput from './UI/ImageInput';
-import TextInput from './UI/TextInput';
+import React, { useRef } from "react";
+import Button from "./UI/Button";
+import FileInput from "./UI/ImageInput";
+import TextInput from "./UI/TextInput";
 import cl from "../styles/components/AddCategoryForm.module.sass";
+import { CategoryType } from "../types/CategoryType";
+import axios from "axios";
 
-const AddCategoryForm = () => {
+
+
+interface AddCategoryFormProps {
+  type: CategoryType;
+}
+
+const AddCategoryForm: React.FC<AddCategoryFormProps> = ({ type }) => {
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+
+  const onSubmit = async() => {
+    const formData = new FormData(formRef.current!);
+    formData.append("type", type);
+    try {
+      const { data } = await axios.post("/api/category", formData);  
+      console.log(data);
+      
+      return data
+    } catch (error: any) {
+      console.log(error.response.data);
+    }
+  };
+
   return (
-    <form className={cl.form}>
+    <form ref={formRef} className={cl.form}>
       <h2>Добавить категорию</h2>
       <br />
-      <TextInput placeholder="Название категории" />
+      <TextInput name="category" id="name" placeholder="Название категории" />
       <br />
       <br />
       <h3>Изоброжение обложки:</h3>
       <br />
-      <FileInput id="file" />
+      <FileInput name="file" />
       <br />
-      <Button>Добавить</Button>
+      <Button onClick={onSubmit}>Добавить</Button>
     </form>
   );
-}
+};
 
-export default AddCategoryForm
+export default AddCategoryForm;
