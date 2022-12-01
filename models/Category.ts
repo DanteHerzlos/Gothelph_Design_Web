985edb627382482d45be6d60cd69acc5eede7815
DanteHerzlos <streat13@gmail.com>
2022-12-02 02:14:02 +0600

    add mongoose models and connect

---
+++ models/Category.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+  title: {
+    // Category title
+    type: String,
+    required: [true, "Please provide a name of the category."],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+  type: {
+    // Product type
+    type: String,
+  },
+  url_img: {
+    // Long product title
+    type: String,
+  },
+  products: [{
+    type: Schema.Types.ObjectId,
+    ref: "Product"
+  }]
+});
+
+export default mongoose.models.Category ||
+  mongoose.model("Category", CategorySchema);
---
+++ models/Product.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    // Short product title
+    type: String,
+    required: [true, "Please provide a name of the product."],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+  long_title: {
+    // Long product title
+    type: String,
+  },
+  body: {
+    // Description product
+    type: String,
+  },
+  sizes: {
+    // List of sizes
+    type: Array,
+  },
+  price: {
+    // Price of product
+    type: String,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+});
+
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
