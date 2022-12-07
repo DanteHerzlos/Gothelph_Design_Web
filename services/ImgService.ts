71c8371ad24e8d602d82e36a5f7532cd1df4fb53
DanteHerzlos <streat13@gmail.com>
2022-12-07 18:39:00 +0600

    add img service and change product srvice

---
+++ services/ImgService.ts
@@ -0,0 +1,11 @@
+import { $authHost, $host } from ".";
+import { IImage } from "../types/IImage";
+
+export default class ImgService {
+  static async getImgs(type: string): Promise<IImage[]> {
+    return await $host(`/img?type=${type}`);
+  }
+  static async postImg(fd: FormData): Promise<IImage> {
+    return await $authHost(`/img`, { body: fd, method: "post" });
+  }
+}
--- services/ProductService.ts
+++ services/ProductService.ts
@@ -2,15 +2,17 @@ import { IProduct } from "../types/IProduct";
 import { $authHost, $host } from "./index";
 
 export default class ProductService {
+  static async getProductById(id: string): Promise<IProduct> {
+    return await $host(`/product/${id}`);
+  }
   static async getProductsByCategory(category: string): Promise<IProduct[]> {
     return await $host(`/product?category=${category}`);
   }
   static async postProduct(fd: FormData): Promise<IProduct> {
     return await $authHost(`/product`, { method: "post", body: fd });
-
   }
   static async removeProduct(id: string) {
-    return await $authHost(`/product/${id}`, {method: 'delete'});
+    return await $authHost(`/product/${id}`, { method: "delete" });
   }
   static async updateProduct(id: string, fd: FormData) {
     return await $authHost(`/product/edit/${id}`, { method: "put", body: fd });
