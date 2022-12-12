767237767a306fc7f1b853f04b2164b63f1a5b0c
DanteHerzlos <streat13@gmail.com>
2022-12-12 17:16:17 +0600

    add active sizes and add orderForm

--- components/ItemInfo.tsx
+++ components/ItemInfo.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import cl from "../styles/components/ItemInfo.module.sass";
 import { IProduct } from "../types/IProduct";
+import OdrerForm from "./forms/OdrerForm";
 import Button from "./UI/Button";
 
 interface ItemInfoProps {
-  product: IProduct
+  product: IProduct;
 }
 
-const ItemInfo:React.FC<ItemInfoProps> = ({product}) => {
+const ItemInfo: React.FC<ItemInfoProps> = ({ product }) => {
+  const [activeSize, setActiveSize] = useState(-1);
+
   return (
     <div className={cl.body}>
       <div className={cl.title}>
@@ -22,7 +25,12 @@ const ItemInfo:React.FC<ItemInfoProps> = ({product}) => {
           <div className={cl.sizes_btns}>
             {product.sizes.length &&
               product.sizes.map((size, index) => (
-                <Button key={index} className={cl.sizes_btns__btn} white>
+                <Button
+                  onClick={() => setActiveSize(index)}
+                  key={index}
+                  className={cl.sizes_btns__btn}
+                  white={activeSize !== index}
+                >
                   {size}
                 </Button>
               ))}
@@ -32,9 +40,17 @@ const ItemInfo:React.FC<ItemInfoProps> = ({product}) => {
         <></>
       )}
       <div>
-        <Button className={cl.buy_btn} white>
+        <OdrerForm
+          className={cl.buy_btn}
+          product_name={
+            activeSize !== -1
+              ? product.long_title + " " + product.sizes![activeSize]
+              : product.long_title
+          }
+          product_price={product.price}
+        >
           купить
-        </Button>
+        </OdrerForm>
       </div>
       <div className={cl.info}>
         <h3>{product.long_title}</h3>
