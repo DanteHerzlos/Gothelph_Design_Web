273f441747bad9641ff1405688d68aab8cb9fd08
DanteHerzlos <streat13@gmail.com>
2022-12-13 23:07:33 +0600

    add edit panel

--- components/ImgSplitSlider.tsx
+++ components/ImgSplitSlider.tsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from "react";
 import LeftIcon from "./Icons/LeftIcon";
 import RightIcon from "./Icons/RightIcon";
 import Image from "next/image";
+import EditCategoryPanel from "./EditCategoryPanel";
+import { CategoryType } from "types/CategoryType";
 import { ICategory } from "types/ICategory";
 import cl from "@styles/components/ImgSplitSlider.module.sass";
+
 
 interface ImgSplitSliderProps {
   categories: ICategory[];
   activeCategory?: ICategory | null;
+  type: CategoryType;
 }
 
 const ImgSplitSlider: React.FC<ImgSplitSliderProps> = ({
   activeCategory,
   categories,
+  type,
 }) => {
   const [active, setActive] = useState<number>(0);
   const [previous, setPrevious] = useState<number>(categories.length - 1);
@@ -39,6 +44,15 @@ const ImgSplitSlider: React.FC<ImgSplitSliderProps> = ({
   return (
     <div className={cl.container}>
       <div className={cl.slider}>
+        {categories.length !== 0 && categories[active] && (
+          <EditCategoryPanel
+            className={cl.edit_panel}
+            type={type}
+            category={categories[active]}
+            editBtn
+            deleteBtn
+          />
+        )}
         <div className={cl.btns}>
           <div onClick={onLeftClick} className={cl.btn_left}>
             <LeftIcon />
@@ -61,7 +75,7 @@ const ImgSplitSlider: React.FC<ImgSplitSliderProps> = ({
               }
               key={index}
               src={category.url_img}
-              alt=""
+              alt={category.title}
             />
           ))}
         </div>
@@ -77,8 +91,7 @@ const ImgSplitSlider: React.FC<ImgSplitSliderProps> = ({
               }
               key={index}
             >
-              <h2>{category.title}</h2>
-              <br />
+              <h1 className={cl.body__title}>{category.title}</h1>
               <p>{category.body}</p>
             </div>
           ))}
