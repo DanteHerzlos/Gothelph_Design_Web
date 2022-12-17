e29e0b3919f8525e6bfb6bc0507a627b3b7a6ec4
DanteHerzlos <streat13@gmail.com>
2022-12-17 20:07:25 +0600

    add delivery info component

---
+++ components/DeliveryInfo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import cl from "@styles/components/DeliveryInfo.module.sass";
+
+const DeliveryInfo = () => {
+  return (
+    <div className={cl.delivery_info}>
+      <h2> ДОСТАВКА В МОСКВА</h2>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec nulla
+        orci. Sed sodales eros in tempus mollis. Pellentesque lobortis arcu at
+        tincidunt vehicula. Integer quis eros turpis. Fusce ut porta massa, in
+        tempor ex. Aliquam aliquet, tellus eu efficitur semper, odio dolor
+        aliquet arcu, nec consectetur magna eros at nulla. Morbi cursus justo in
+        ultrices fringilla.
+      </p>
+    </div>
+  );
+}
+
+export default DeliveryInfo
--- pages/clothes/[category]/[itemId].tsx
+++ pages/clothes/[category]/[itemId].tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from "next";
 import ClothesLayout from "@components/layouts/ClothesLayout";
 import GallerySlider from "@components/GallerySlider";
 import ItemInfo from "@components/ItemInfo";
+import DeliveryInfo from "@components/DeliveryInfo";
 import { IProduct } from "types/IProduct";
 import cl from "@styles/ProductItem.module.sass";
 
@@ -18,18 +19,7 @@ const ClothesItem: React.FC<ClothesItemProps> = ({ product }) => {
           <ItemInfo product={product} />
         </GallerySlider>
       </div>
-      <div className={cl.delivery_info}>
-        <h2> ДОСТАВКА В МОСКВА</h2>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec
-          nulla orci. Sed sodales eros in tempus mollis. Pellentesque lobortis
-          arcu at tincidunt vehicula. Integer quis eros turpis. Fusce ut porta
-          massa, in tempor ex. Aliquam aliquet, tellus eu efficitur semper, odio
-          dolor aliquet arcu, nec consectetur magna eros at nulla. Morbi cursus
-          justo in ultrices fringilla.
-        </p>
-      </div>
+      <DeliveryInfo />
     </ClothesLayout>
   );
 };
